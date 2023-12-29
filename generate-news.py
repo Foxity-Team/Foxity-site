c3c0369070a3a5fdc3fa9a10eebfbe563ce80382
Griss <grisshink@mail.ru>
2023-12-29 22:28:43 +0300

    Format fix

--- generate-news.py
+++ generate-news.py
@@ -14,10 +14,10 @@ def readDirectoryRec(dirPath: os.PathLike) -> Iterable:
     return map(lambda v: (v[0], readDirectoryRec(v[0])) if path.isdir(v[0]) else v, files)
 
 def filterFiles(pred: Callable[[str, str], bool], dir: Iterable) -> Iterable:
-    return filter(lambda v: v is not None, 
-                  map(lambda v: (v[0], filterFiles(pred, v[1])) if not isinstance(v[1], str) 
-                                else (v if pred(v[0], v[1])
-                                      else None), dir))
+    filtered = map(lambda v: (v[0], filterFiles(pred, v[1])) 
+                             if not isinstance(v[1], str) 
+                             else (v if pred(v[0], v[1]) else None), dir)
+    return filter(lambda v: v is not None, filtered)
 
 def mapFiles(pred: Callable[[str, str], str], dir: Iterable) -> Iterable:
     return map(lambda v: (v[0], mapFiles(pred, v[1])) if not isinstance(v[1], str) else (v[0], pred(v[0], v[1])), dir)
@@ -44,9 +44,6 @@ def directoryToHtml(dir: str) -> str:
                  filter(lambda v: v != 'index.html' and not path.isdir(path.join(dir, v)), os.listdir(dir))))
     return f'<ol>{listing}</ol>'
 
-def wrapWithDirectoryListing(template: str, filename: str) -> str:
-    return template.replace('{{ body }}', directoryToHtml(path.dirname(filename)))
-
 def wrapWithTemplate(template: str, filename: str) -> str:
     file = readAll(filename)
     return template.replace('{{ body }}', file if file != '' and file.splitlines()[0] != '<!-- DirList -->' 
@@ -69,11 +66,11 @@ allowedExts = ['.md', '.html']
 
 template = readAll('templates/index.html')
 
-val = collectDict(
+val = collectDict (
       mapFilenames(lambda f   : f if path.splitext(f)[1] == '' else path.splitext(f)[0] + '.html',
       mapFilenames(lambda f   : distPath + f.removeprefix(pagesPath),
-      mapFiles(    lambda f, d: wrapWithTemplate(template, f), 
-      filterFiles( lambda f, d: path.splitext(f)[1] in allowedExts, readDirectoryRec(pagesPath))))))
+      mapFiles    (lambda f, d: wrapWithTemplate(template, f), 
+      filterFiles (lambda f, d: path.splitext(f)[1] in allowedExts, readDirectoryRec(pagesPath))))))
 
 counter = 0
 
