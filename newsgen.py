6dcebdcbb34c514ad2612de76341caa906b996e7
ebalaysinc <151670861+ebalaysinc@users.noreply.github.com>
2025-05-05 18:30:22 +0300

    Minor changes (check PR for details)

--- newsgen.py
+++ newsgen.py
@@ -1,18 +1,16 @@
 from markdown import markdown
 from os import listdir
 
-def renderNews():
+def render_news():
     news = []
     files = listdir("news/")
-    files.sort(key=max)
 
     for file in files:
         index = int(file.split("#")[0])
-        post = open("news/"+file, "r").read()
+        post = open("news/"+file, "r", encoding='utf-8').read()
 
         news.append((
             index,
             markdown(post)
         ))
-    
     return sorted(news, key=lambda x: x[0], reverse=True)
